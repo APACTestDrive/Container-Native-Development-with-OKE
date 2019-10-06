6478e6d4d71072884030cbd610b37486b214f40e
Kwan Wan <kwan@Kwan-MacBook-Pro.local>
2019-10-06 22:20:03 +0800

    Removed code

--- dbconfig.js
+++
@@ -1,6 +0,0 @@
-module.exports= {
-dbuser: 'admin', 
-dbpassword: 'foReVER@4Stupid', 
-connectString: 'mcdemodb_tp' ,
-walletpwd: 'welcome1'
-}
--- web.js
+++
@@ -1,6 +0,0 @@
-var PORT = process.env.PORT || 8080;
-var express = require("express");
-var app = express();
-app.use(express.static('web'));
-app.listen(PORT);
-console.log("Server started in port:" + PORT);
